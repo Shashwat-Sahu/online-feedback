83d83c930f33fe095cc0aa14e58b9608d460730a
samiksha-dev <69921661+samiksha-dev@users.noreply.github.com>
2024-01-09 20:59:16 +0530

    Update FeedbackForm.jsx

--- src/Pages/User/FeedbackForm.jsx
+++ src/Pages/User/FeedbackForm.jsx
@@ -3,14 +3,31 @@ import { Col,Row,Container } from "react-bootstrap";
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 const currDate=new Date().toLocaleDateString();
 const FeedbackForm = () => {
   return (
     <div>
-        <Container className='d-flex' >
+        <Container className='d-flex' style={{margin:"0"}} >
 <Row>
+<Col className='d-flex justify-content-center align-items-center col-auto bg-dark min-vh-100' style={{color:"white",opacity:"0.3"}}>
+        <div>
+          <AccountCircleIcon style={{fontSize:"xxx-large"}}/>
+          <h1>Samiksha Sahu</h1>
+          <h5> Department:Engineering</h5>
+          <h6>Service ID:78908</h6>
+          <Form.Select size="sm" style={{width:"auto",backgroundColor:"#fff3cd8c"}}>
+      <option>Select Student</option>
+      <option value="1">Jainendra Kr Sahu:17116</option>
+      <option value="2">Neelam Sahu:22027</option>
+      <option value="3">Shashwat Sahu:25112</option>
+    </Form.Select>
+    
+        </div>
+        </Col>
+        <Col>
     <h1 style={{textAlign:"center"}}>User Feedback Form</h1>
         {/* <Col style={{paddingRight:"0"}}> */}
     <Table striped bordered style={{width:"auto",textAlign:"none",marginTop:"2rem"}}>
@@ -66,7 +83,7 @@ const FeedbackForm = () => {
         <Form.Control as="textarea" aria-label="With textarea" />
       </InputGroup>
       </div>
-   
+      </Col>
     {/* </Col> */}
     </Row>
     </Container>
