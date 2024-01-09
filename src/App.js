e104bfef99de18374f9c22dd2f897ac611072045
samiksha-dev <69921661+samiksha-dev@users.noreply.github.com>
2024-01-09 20:59:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login';
-import User from './Pages/User'
+import FeedbackFrom from './Pages/User/FeedbackForm';'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
