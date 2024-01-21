ea86683785540d6f8e3a0a3d1da3dd4439f21f50
shashwat sahu <shaswatsahu25@gmail.com>
2024-01-21 23:58:48 +0530

    vercel update

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.use("/counselee", require("./Routes/Counselee.js"))
 const PORT = process.env.PORT || 8000
 
 
-app.use(express.static('./build'))
 const path = require('path')
+app.use(express.static(path.join(__dirname, 'build')))
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
